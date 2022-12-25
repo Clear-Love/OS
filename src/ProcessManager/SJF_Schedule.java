18f09f1f589d7055a4dc53cd2ddc379f694a09fb
Clear-Love <2091319361@qq.com>
2022-12-25 20:30:34 +0800

    短作业优先调度优化，现在更短的作业可以抢夺cpu的控制权

--- src/ProcessManager/SJF_Schedule.java
+++ src/ProcessManager/SJF_Schedule.java
@@ -1,5 +1,6 @@
 package ProcessManager;
 
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -9,11 +10,11 @@ import java.util.List;
  * @modified lmio
  * @version 2.0
  */
-public class SJF_Schedule extends PCBList implements Scheduler, Runnable{
+public class SJF_Schedule extends PCBList implements Scheduler{
 
     public SJF_Schedule(List<PCB> pcbList) {
         super(pcbList);
-        readyQueue.sort((p1, p2) -> p2.getBurstTime() - p1.getBurstTime());
+        readyQueue.sort(Comparator.comparingInt(PCB::getBurstTime));
     }
 
     // 实现Scheduler接口的schedule()方法
@@ -25,9 +26,7 @@ public class SJF_Schedule extends PCBList implements Scheduler, Runnable{
         // 循环调度就绪队列中的进程
         while (!readyQueue.isEmpty()) {
             // 找到周期最短的进程
-
             PCB pcb = readyQueue.get(0);
-            int shortestTime = pcb.getBurstTime();
 
             // 启动进程
             PCB_start(pcb);
@@ -40,21 +39,21 @@ public class SJF_Schedule extends PCBList implements Scheduler, Runnable{
 
     @Override
     public void insertProcess(PCB pcb) {
-        int priority = pcb.getPriority();
-        synchronized (this){
-            int newTime = pcb.getPriority();
-            if(pcb.getPriority() < readyQueue.get(0).getPriority()){
+        System.out.println("进程" + pcb.getId() + "插入");
+        int newTime = pcb.getBurstTime();
+        if(newTime < readyQueue.get(0).getBurstTime()){
+            synchronized (this){
                 //将正在运行的进程转到就绪
                 readyQueue.get(0).setStatus(PCB.ProcessStatus.READY);
                 //插入到队列前面
                 readyQueue.add(0, pcb);
-            }else {
-                //用插入排序使就绪队列重新排序
-                for (int i = readyQueue.size(); i > 0; i--) {
-                    int time = readyQueue.get(i).getPriority();
-                    if(newTime > time){
-                        readyQueue.add(i+1, pcb);
-                    }
+            }
+        }else {
+            //用插入排序使就绪队列重新排序
+            for (int i = readyQueue.size()-1; i >= 0; i--) {
+                int time = readyQueue.get(i).getBurstTime();
+                if(newTime > time){
+                    readyQueue.add(i+1, pcb);
                 }
             }
         }
